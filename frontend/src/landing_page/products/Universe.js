81ba3a036f87c9fce582f693df5f555a19683573
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-25 11:38:21 +0530

    hiding url by environment(.env) for deployment

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import useAuthStatus from "../auth/useAuthStatus";
+import { DASHBOARD_URL } from "../../config/urls";
 function Universe() {
  const { isCheckingAuth, isAuthenticated } = useAuthStatus();
   return (
@@ -89,7 +90,7 @@ function Universe() {
         {isCheckingAuth ? null : isAuthenticated ? (
                     <a
                         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-lg mb-12'
-                        href="http://localhost:3001"
+                href={DASHBOARD_URL}
                     >
                         Go to Dashboard
                     </a>
